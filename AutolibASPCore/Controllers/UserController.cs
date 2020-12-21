6582cb230880e6880f49e51ac9c9640862e92ef7
Watchtux <guillettheodore@gmx.com>
2020-12-21 19:27:33 +0100

    Modification base

--- AutolibASPCore/Controllers/UserController.cs
+++ AutolibASPCore/Controllers/UserController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using AutolibASPCore.Models.Domain;
+using CryptoHelper;
 
 namespace AutolibASPCore.Controllers
 {
@@ -14,6 +16,8 @@ namespace AutolibASPCore.Controllers
         }
         public IActionResult Login()
         {
+            // var hash = Crypto.HashPassword("d6506c7bcf0d483354d2eca58ed6611ea11a134a13e7d1940b3ce549ec024943");
+            // Crypto.VerifyHashedPassword(hash, "test");
             return View();
         }
         public IActionResult Logout()
