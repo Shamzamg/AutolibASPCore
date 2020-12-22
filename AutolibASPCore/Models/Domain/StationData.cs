0b4ec415fea18079446a01f6f209e043ae018a00
p1718423 <shamsdine.ziani@etu.univ-lyon1.fr>
2020-12-22 13:52:53 +0100

    Liste des stations et stations V1

---
+++ AutolibASPCore/Models/Domain/StationData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AutolibASPCore.Models.Domain
+{
+    public partial class StationData
+    {
+
+            public Borne Borne { get; set; }
+            public Station Station { get; set; }
+            public Vehicule Vehicule { get; set; }
+            public TypeVehicule TypeVehicule { get; set; }
+    }
+}
